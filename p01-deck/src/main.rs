7c5b381d477fc2f4c78a4c4b6a6f5a098e97bcd8
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-07-26 23:17:37 -0400

    Initial commit

---
+++ p01-deck/src/main.rs
@@ -0,0 +1,30 @@
+/*
+    objective:
+
+    make a data object that can represent a deck of cards
+
+    give it a new() method that creates a set of 52 cards
+    give it a shuffle() method that randomizes the order of the cards
+    give it a deal() method that returns one card
+
+    this can be represented in a struct, which in rust is similar to classes in other languages
+*/
+
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>, //vectors can grow and shrink in size, while arrays have fixed lengths
+}
+
+fn main() {
+    //in Rust, variables are referred to as 'bindings'
+    //another equivalent way to create the empty vector would be Deci { cards: Vec::new() }
+    let deck = Deck { cards: vec![] }; 
+
+    //the :? inside the {} is the Debug formatter
+    //which needs #[derive(Debug)] added to the struct Deck to make function without an error
+    //but now it is able to print this struct to the console
+    println!("Here's your deck: {:?}", deck);
+    
+    // another way of expressing that formatted string is to put the binding name in the {}
+    // println!("Here's your deck: {deck}");
+}
