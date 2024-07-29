bb881d145909f921661c9de0294484eae625e16b
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-07-29 02:49:26 -0400

    Add further explanation on match statement usage

--- p03-media/src/main.rs
+++ p03-media/src/main.rs
@@ -7,6 +7,9 @@ enum Media {
 
 impl Media {
     fn description(&self) -> String {
+        //Rust will not allow you to process the properties of self, like self.title, until the type has been first determined,
+        //even though all three variants have a .title property
+
         //a verbose way to do type checking to find out what variant of the Media Enum this is
         // if let Media::Book { title, author } = self {
         //     format!("Book: {} by {}", title, author)
@@ -19,14 +22,25 @@ impl Media {
         // }
 
         //the less verbose match statement way of doing the type checking for which variant it is:
+        // match self {
+        //     Media::Book { title, author } => format!("Book: {} by {}", title, author),
+        //     Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
+        //     Media::Audiobook { title } => format!("Audiobook: {}", title),
+        // }
+
+        //self is being checked against the type pattern mentioned on the left side of the => arrow,
+        //if self matches the structure of any of the given Enum variants, then the code on the right of the => is executed
+        //which in this case, looks to be an implicit return statement of the format!() macro
+
+        //it is also possible to wrap curly braces around the statement after the => arrow, which allows multiline blocks of statements
         match self {
-            Media::Book { title, author } => format!("Book: {} by {}", title, author),
-            Media::Movie { title, director } => format!("Movie: {} by {}", title, director),
-            Media::Audiobook { title } => format!("Audiobook: {}", title),
+            Media::Book { title, author } => {format!("Book: {} by {}", title, author)},
+            Media::Movie { title, director } => {format!("Movie: {} by {}", title, director)},
+            Media::Audiobook { title } => {format!("Audiobook: {}", title)},
         }
+
     }
 }
-
 
 fn main() {
     let audiobook = Media::Audiobook {
@@ -38,7 +52,7 @@ fn main() {
     };
     let book = Media::Book {
         title: String::from("A Book"),
-        author: String::from("Booker Authorman"),
+        author: String::from("Author Authorman"),
     };
 
     println!("{}", audiobook.description());
