1b1facf271bfcdea2c34ad894ab080436f7512ce
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-07-29 01:35:39 -0400

    Setup of initial Enum concept demonstration

---
+++ p03-media/src/main.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+}
+
+fn print_media(media: Media){
+    println!("{:#?}", media);
+}
+fn main() {
+    let audiobook = Media::Audiobook { title: String::from("An Audiobook") };
+    print_media(audiobook);
+}
