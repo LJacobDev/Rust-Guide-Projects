8acc3e39dbf0771518b92f996c19afdd335f4882
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-07-28 20:21:33 -0400

    checkpoint before branching off to attempt my own completion before watching guide implementation

--- p02-bank/src/main.rs
+++ p02-bank/src/main.rs
@@ -39,18 +39,36 @@ fn main() {
 
     println!("Bank {bank:#?}");
     
-    //printing the account once, no error
-    print_account(account);
-    //printing the account a second time, an error occurs
-    //it has something to do with the ownership / borrowing feature in rust and this is not seen in other programming languages usually
-    print_account(account);
+
+
     /*
-        error information:
-        use of moved value: `account`
-        value used here after moverustcClick for full compiler diagnostic
-        main.rs(43, 19): value moved here
-        main.rs(38, 9): move occurs because `account` has type `Account`, which does not implement the `Copy` trait
-        main.rs(29, 27): consider changing this parameter type in function `print_account` to borrow instead if owning the value isn't necessary
-        main.rs(2, 1): if `Account` implemented `Clone`, you could clone the valu
+
+        The following code is being left as a reminder comment
+
+        This was the main point of this project, to demonstrate
+        that Rust has ownership, borrowing, lifetime features
+        Most of the videos were about showing examples of it and
+        doing exercises to practice understanding of it
+
+        before watching the final videos where he shows implementing the functions,
+        I wanted to try doing them myself first in a branch and then depending on
+        how close my version is to what he shows, I'll either merge the branch over
+        or else leave it as an unmerged branch and go on to make the main branch
+        follow the guide's examples
+
+        //printing the account once, no error
+        print_account(account);
+        //printing the account a second time, an error occurs
+        //it has something to do with the ownership / borrowing feature in rust
+        print_account(account);
+        /*
+            error information:
+            use of moved value: `account`
+            value used here after moverustc Click for full compiler diagnostic
+            main.rs(43, 19): value moved here
+            main.rs(38, 9): move occurs because `account` has type `Account`, which does not implement the `Copy` trait
+            main.rs(29, 27): consider changing this parameter type in function `print_account` to borrow instead if owning the value isn't necessary
+            main.rs(2, 1): if `Account` implemented `Clone`, you could clone the valu
+        */
     */
 }
