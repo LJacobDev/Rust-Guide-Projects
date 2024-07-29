a882a4930fbb919c464f831415cb19937a0eeb23
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-07-28 22:11:44 -0400

    Completed implementation using self-completed attempt before viewing guide implementation

--- p02-bank/src/main.rs
+++ p02-bank/src/main.rs
@@ -1,3 +1,11 @@
+/*
+    Objective:  Simulate a Bank object that can contain multiple Account objects, which can have balances.
+                These objects will also have functionality to add accounts, add funds, withdraw funds, summarize status.
+
+                The intention of the project is to demonstrate Rust's ownership, borrowing, and lifetime features,
+                and to understand how data moves around inside of a Rust application differently than in other languages
+*/
+
 #[derive(Debug)]
 struct Account {
     id: u32,
@@ -7,6 +15,7 @@ struct Account {
 
 impl Account {
     fn new(id: u32, holder: String) -> Self {
+        println!("New account created with ID: {} for Holder: {}", id, holder);
         Account {
             id,
             holder,
@@ -30,8 +39,7 @@ impl Account {
             println!("Withdrawing {}", withdrawal);
             self.balance -= withdrawal;
             println!("Withdrawal complete, new balance: {}", self.balance);
-        }
-        if withdrawal > self.balance {
+        } else if withdrawal > self.balance {
             println!("Insufficient funds");
         }
         self.balance
@@ -52,10 +60,37 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    ///Takes a given Account instance and adds it to the Bank's accounts vector.
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
+
+    ///Returns the sum of all the accounts in the Bank
+    fn sum_accounts(&self) -> isize {
+        let mut sum = 0;
+
+        for account in &self.accounts {
+            sum += account.balance;
+        }
+
+        sum
+    }
+
+    ///Returns a Vector<String> containing the summaries of each Account contained in the Bank.
+    fn accounts_summary(&self) -> Vec<String> {
+        let mut summary = vec![];
+
+        for account in &self.accounts {
+            summary.push(account.account_summary());
+        }
+
+        summary
+    }
 }
 
 fn main() {
-    let bank = Bank::new();
+    let mut bank = Bank::new();
 
     //putting in a string like "" is seen as &str, or a string slice,
     //so to get a true String type you need to use String::from(), or format!()
@@ -64,14 +99,30 @@ fn main() {
     println!("{}", account.account_summary());
 
     account.credit_funds(10);
-
     account.debit_funds(5);
-
     account.debit_funds(15);
 
     println!("{}", account.account_summary());
 
-    println!("Bank {bank:#?}");
+    bank.add_account(account);
+
+    //account binding is empty now, fill it with another new Account instance
+    account = Account::new(2, String::from("Jeffy"));
+    account.credit_funds(100);
+    bank.add_account(account);
+
+    bank.accounts[1].credit_funds(15);
+
+    let bank_sum = bank.sum_accounts();
+    println!("Sum of Bank's accounts: {}", bank_sum);
+
+    //It is interesting to see that the bank.accounts_summary() output to the console shows the holder name with an escape character.
+    //The direct account.account_summary() output doesn't print as having escape characters, but it also doesn't have
+    //double quotes wrapped around its output.
+    //But when it appears in the bank.accounts_summary() as a vector of strings, there are double quotes
+    //wrapped around each element in the output, and then there are escape characters showing in the inner quotes
+    //around the holder name that were not intentionally placed there but were added automatically
+    println!("Bank details:\n {:#?}", bank.accounts_summary());
 
     /*
 
@@ -83,10 +134,7 @@ fn main() {
         doing exercises to practice understanding of it
 
         before watching the final videos where he shows implementing the functions,
-        I wanted to try doing them myself first in a branch and then depending on
-        how close my version is to what he shows, I'll either merge the branch over
-        or else leave it as an unmerged branch and go on to make the main branch
-        follow the guide's examples
+        I wanted to try doing them myself first before viewing and following the guide's examples
 
         //printing the account once, no error
         print_account(account);
@@ -96,7 +144,7 @@ fn main() {
         /*
             error information:
             use of moved value: `account`
-            value used here after moverustc Click for full compiler diagnostic
+            value used here after move rustc Click for full compiler diagnostic
             main.rs(43, 19): value moved here
             main.rs(38, 9): move occurs because `account` has type `Account`, which does not implement the `Copy` trait
             main.rs(29, 27): consider changing this parameter type in function `print_account` to borrow instead if owning the value isn't necessary
