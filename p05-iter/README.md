0f6c9c902a6e77dce6e0d262549585e86e429148
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-08-25 15:43:27 -0400

    Add example of iter().for_each() Iterator Consumer

---
+++ p05-iter/README.md
@@ -0,0 +1,3 @@
+# p05-iter
+
+This is a simple project folder meant to demonstrate Iterators in Rust
--- p05-iter/src/main.rs
+++ p05-iter/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("red"),
+        String::from("green"),
+        String::from("blue"),
+    ];
+
+    //example of an Iterator struct being derived from a Vector (it can be used on other data structures as well)
+
+    // color_iter needs to be mutable because it has a cursor value that changes when .next() is invoked
+
+    let mut color_iter = colors.iter();
+
+    println!("{:?}", color_iter.next());    //prints "red"
+    println!("{:?}", color_iter.next());    //prints "green"
+    println!("{:?}", color_iter.next());    //prints "blue"
+    println!("{:?}", color_iter.next());    //prints None
+
+
+    //print all elements in the vector using an Iterator and Iterator consumer
+    print_elements(colors);
+}
+
+///Example to demonstrate the .iter().for_each() Iterator Consumer method
+fn print_elements(data: Vec<String>) {
+    data.iter().for_each(|el| println!("{}", el))
 }
