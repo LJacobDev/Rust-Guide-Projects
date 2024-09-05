90be8ef7f79930114c06c6b89732b50e5df65ae1
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-05 19:52:18 -0400

    Add flexible mixed type function

--- p07-generics/src/main.rs
+++ p07-generics/src/main.rs
@@ -14,7 +14,12 @@ fn main() {
     let d: f64 = 4.0;
 
     println!("{}", solve_flexible(c,d));
-    
+
+    let e: f32 = 3.0;
+    let f: f64 = 4.0;
+
+    println!("{}", solve_flexible_mixed_float(e, f));
+
 }
 
 
@@ -23,10 +28,20 @@ fn solve(a: f64, b: f64) -> f64 {
 }
 
 ///make another version that can take f32 or f64
+///but for this one, both arguments have to be the same type
 fn solve_flexible<T: Float>(a: T, b: T) -> f64 {
 
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+///make another version that can take a mixed combination of f32 and f64
+fn solve_flexible_mixed_float<T: Float, U: Float> (a: T, b: U) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+
 }
