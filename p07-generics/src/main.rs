3739a4377259670244d777830f8d29f5b389a28f
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-04 20:15:34 -0400

    Example of using num_traits to make function that can take two f32s or two f64s

--- p07-generics/src/main.rs
+++ p07-generics/src/main.rs
@@ -1,11 +1,32 @@
+use num_traits::Float;
 
 fn main() {
 
     println!("{}", solve(3.0,4.0));
+
+
+    let a: f32 = 3.0;
+    let b: f32 = 4.0;
+
+    println!("{}", solve_flexible(a,b));
+
+    let c: f64 = 3.0;
+    let d: f64 = 4.0;
+
+    println!("{}", solve_flexible(c,d));
     
 }
 
 
 fn solve(a: f64, b: f64) -> f64 {
     (a.powi(2) + b.powi(2)).sqrt()
+}
+
+///make another version that can take f32 or f64
+fn solve_flexible<T: Float>(a: T, b: T) -> f64 {
+
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
