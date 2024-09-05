072da9dc841bee0fee7d41053bff7aa4335bf2c3
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-05 19:57:40 -0400

    Add any number type function

--- p07-generics/src/main.rs
+++ p07-generics/src/main.rs
@@ -1,27 +1,28 @@
-use num_traits::Float;
+use num_traits::{Float, ToPrimitive};
 
 fn main() {
-
-    println!("{}", solve(3.0,4.0));
-
+    println!("{}", solve(3.0, 4.0));
 
     let a: f32 = 3.0;
     let b: f32 = 4.0;
 
-    println!("{}", solve_flexible(a,b));
+    println!("{}", solve_flexible(a, b));
 
     let c: f64 = 3.0;
     let d: f64 = 4.0;
 
-    println!("{}", solve_flexible(c,d));
+    println!("{}", solve_flexible(c, d));
 
     let e: f32 = 3.0;
     let f: f64 = 4.0;
 
     println!("{}", solve_flexible_mixed_float(e, f));
 
-}
+    let g: i32 = 3;
+    let h: f64 = 4.0;
 
+    println!("{}", solve_any_number_type(g, h));
+}
 
 fn solve(a: f64, b: f64) -> f64 {
     (a.powi(2) + b.powi(2)).sqrt()
@@ -30,7 +31,6 @@ fn solve(a: f64, b: f64) -> f64 {
 ///make another version that can take f32 or f64
 ///but for this one, both arguments have to be the same type
 fn solve_flexible<T: Float>(a: T, b: T) -> f64 {
-
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
@@ -38,10 +38,17 @@ fn solve_flexible<T: Float>(a: T, b: T) -> f64 {
 }
 
 ///make another version that can take a mixed combination of f32 and f64
-fn solve_flexible_mixed_float<T: Float, U: Float> (a: T, b: U) -> f64 {
+fn solve_flexible_mixed_float<T: Float, U: Float>(a: T, b: U) -> f64 {
     let a_f64 = a.to_f64().unwrap();
     let b_f64 = b.to_f64().unwrap();
 
     (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
 
+///make another version that can take any number using num_traits::ToPrimitive trait bound
+fn solve_any_number_type<T: ToPrimitive, U: ToPrimitive>(a: T, b: U) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
