d93b71c8ced7e49469a790da372c2574447e549b
LJacobDev <143475198+LJacobDev@users.noreply.github.com>
2024-09-05 21:12:07 -0400

    Add initial starting point where basket can hold and retrieve strings

---
+++ p08-traits/src/basket.rs
@@ -0,0 +1,24 @@
+
+pub struct Basket {
+    pub item: Option<String>,
+}
+
+impl Basket {
+    pub fn get(&mut self) -> Option<String> {
+
+        //this .take() is capable of moving the value out of self.item, and leaving a None in its place,
+        //where just returning self.item would generate a moving error by itself
+        //if there is no value in self.item, .take() will return a None, so this method's implementation is complete with this.
+        //.take() is a method of the Option enum, and since item is an Option<String>, it is available to use here
+        self.item.take()
+
+    }
+
+    pub fn put(&mut self, item: String) {
+
+        //in the starting case of using strings only, just assign what is given to put into the item
+        //when numbers are included, additional functionality will be added to be able to add the numbers together
+        self.item = Some(item);
+    }
+}
+
---
+++ p08-traits/src/main.rs
@@ -0,0 +1,46 @@
+use basket::Basket;
+
+mod basket;
+
+fn main() {
+
+/*
+
+    Objective:
+
+    create a Struct called Basket that can hold one value that is of any possible type of data, whether number, string, vector, struct
+
+    give it a .get() method that returns an Option if it has Some value or None
+
+    give it a .put() method that assigns a new value to it, overwriting the prior value, unless it is having a number assigned while it already contains one, in which case, add the numbers together
+
+    give it an .is_empty() method that returns a boolean about whether it contains anything currently
+
+
+    Secondary goal:
+
+    make a Stack Struct that can hold more than one value of various data types
+
+    it can hold any kind of data, but its collections will be homogeneous 
+
+    it has .put() method that adds an additional value to be stores
+
+    it has .get() which returns an Option, with the most recently stored value or a None if Stack is empty
+
+    it has .is_empty()
+
+
+
+    Since these two structs have the same method names with the same method signatures, they can be implemented as a Trait
+
+    This will also enable using a Stack or a Basket interchangeably in any place in the code due to their common Trait type
+
+*/
+
+let mut basket = Basket { item: None};
+
+basket.put(String::from("Stored value"));
+
+println!("{:?}", basket.get());
+
+}
